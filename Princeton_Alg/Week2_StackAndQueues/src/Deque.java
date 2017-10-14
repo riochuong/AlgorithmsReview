178e65a5d3e9bd07c1882d972fbe84b958a6faec
CHUONG DAO <riochuong@gmail.com>
2017-10-14 01:34:40 -0700

    deque implementation is done and tested

---
+++ Princeton_Alg/Week2_StackAndQueues/src/Deque.java
@@ -0,0 +1,151 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    /* declare head tail to help add and remove
+    *  item faster from both ends of the deque
+    * */
+    private Node<Item> head = null;
+    private Node<Item> tail = null;
+    private int size = 0;
+
+    public Deque() {
+
+    }                           // construct an empty deque
+
+    public boolean isEmpty() { return size == 0; }// is the deque empty?
+
+    public int size() { return size; } // return the number of items on the deque
+
+    public void addFirst(Item item) {
+
+        // make sure item is not null
+        if (item == null){ throw new IllegalArgumentException("Item cannot be nukk"); }
+
+        if (isEmpty()) {
+            initDeque(item);
+        }
+        else {
+            // add item to current list
+            Node<Item> newNode = new Node<>();
+            newNode.data = item;
+            newNode.next = head;
+            newNode.prev = null; // new head
+            head.prev = newNode; // connect to new head
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(Item item) {
+        if (item == null){ throw new IllegalArgumentException("Item cannot be nukk"); }
+
+        if (isEmpty()){
+            initDeque(item);
+        } else {
+            Node <Item> newNode = new Node<>();
+            // make tail to point to the new node and reassign tail next to new tail
+            newNode.data = item;
+            newNode.next = null;
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }           // add the item to the end
+
+    public Item removeFirst() {
+        if (isEmpty()) { throw new NoSuchElementException("Deque is empty. Should Check empty current"); }
+        Item res = null;
+        Node<Item> removedNode = head;
+        // dercrease the size
+        if (size == 1) {
+            head = null;
+            tail = null;
+            removedNode.next = null;
+            removedNode.prev = null;
+            res = removedNode.data;
+        }
+        else{
+            head = removedNode.next;
+            removedNode.next = null;
+            head.prev = null;
+            res = removedNode.data;
+        }
+        size--;
+        return res;
+    }                // remove and return the item from the front
+
+    public Item removeLast()  {
+        if (isEmpty()) { throw new NoSuchElementException("Deque is empty. Should Check empty current"); }
+        Item res = null;
+        Node<Item> removedNode = tail;
+        // dercrease the size
+        if (size == 1) {
+            head = null;
+            tail = null;
+            removedNode.next = null;
+            removedNode.prev = null;
+            res = removedNode.data;
+        }
+        else{
+            tail = removedNode.prev;
+            tail.next = null;
+            removedNode.prev = null;
+            res = removedNode.data;
+        }
+        size--;
+        return res;
+    }               // remove and return the item from the end
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }       // return an iterator over items in order from front to end
+
+    public static void main(String[] args) {
+
+    }   // unit testing (optional)
+
+
+    private class Node<Item> {
+        Node<Item> next;
+        Node<Item> prev;
+        Item data;
+    }
+
+    /**
+     * helper to avoid repeat code to initialize deque on addfirst and addlast
+     * @param item
+     */
+    private void initDeque(Item item ){
+        head = new Node<>();
+        head.data = item;
+        head.next = null;
+        head.prev = null;
+        tail = head;
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+
+        private Node<Item> current = head;
+
+        @Override
+        public boolean hasNext() {
+            return (current != null) && (current.next != null);
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.data;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported operation");
+        }
+    }
+
+}
---
+++ Princeton_Alg/Week2_StackAndQueues/src/TestDeque.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+public class TestDeque {
+
+    private static Random randGen = new Random();
+
+    private static void testAddRemoveFirst() {
+        System.out.println("Test Remove First");
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(10);
+        deque.addFirst(9);
+        System.out.println("Total size "+deque.size());
+        int size = deque.size();
+        for (int i = 0; i < size ; i++) {
+            System.out.print(deque.removeFirst()+" ");
+        }
+        System.out.println();
+    }
+
+    private static void testAddFirstRemoveLast() {
+        System.out.println("Test Remove Last");
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(10);
+        deque.addFirst(9);
+        System.out.println("Total size "+deque.size());
+        int size = deque.size();
+        for (int i = 0; i < size ; i++) {
+            System.out.print(deque.removeLast()+" ");
+        }
+        System.out.println();
+    }
+
+
+    private static void testAddRemoveRandom() {
+        System.out.println("Test Remove Last");
+        Deque<Integer> deque = new Deque<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(10);
+        deque.addFirst(9);
+        deque.addLast(19);
+        deque.addLast(21);
+        deque.addFirst(10);
+        System.out.println("Total size "+deque.size());
+        while(!deque.isEmpty()) {
+            if (randGen.nextInt() % 2 == 0) {
+                System.out.println("Remove Last : "+deque.removeLast()+" ");
+            }
+            else {
+                System.out.println("Remove First : "+deque.removeFirst()+" ");
+            }
+        }
+
+        System.out.println();
+    }
+
+
+    public static void main (String[] args) {
+        testAddRemoveFirst();
+        testAddFirstRemoveLast();
+        testAddRemoveRandom();
+    }
+}
